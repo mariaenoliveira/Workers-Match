1fae4157b2647d5fad95fec8e8c2c926a5728971
mariaenoliveira <135348825+mariaenoliveira@users.noreply.github.com>
2024-03-15 11:18:36 -0300

    commit15/03

--- resources/views/patrocinadores.blade.php
+++ resources/views/patrocinadores.blade.php
@@ -20,32 +20,71 @@
         <div class="patrocinador">
             <img name="" src="/image/C6Bank.png" alt="">
             {{-- <h1 id="imagem">WM</h1> --}}
-            <h3 id="info">Informações</h3>
+            <h3 id="info">O C6 Bank é uma instituição financeira brasileira digital que oferece uma variedade de serviços bancários, 
+                incluindo conta corrente, cartões de crédito e débito, investimentos e empréstimos. Fundado em 2019, 
+                o C6 Bank se destaca por sua abordagem inovadora e tecnológica, permitindo que os clientes realizem operações bancárias de forma simples e 
+                conveniente através de seu aplicativo móvel. Com um foco na experiência do usuário e na oferta de soluções financeiras modernas, 
+                o C6 Bank tem ganhado espaço no mercado financeiro brasileiro.</h3>
         </div>
         <div class="patrocinadorDireita">
             <img name="" src="/image/GovernoFederal.jpg" alt="">
             {{-- <h1 id="imagem">WM</h1> --}}
-            <h3 id="info">Informações</h3>
+            <h3 id="info">O governo federal refere-se à estrutura governamental central de um país, 
+                responsável pela administração das questões nacionais em nível nacional. Em muitos países, incluindo os Estados Unidos, o Brasil e a Alemanha, 
+                o governo federal é composto por três poderes distintos: executivo, legislativo e judiciário. 
+                O poder executivo é liderado pelo chefe de Estado ou chefe de governo, como o presidente ou primeiro-ministro, 
+                e é responsável pela implementação e execução das leis. O poder legislativo é composto pelo parlamento ou congresso, 
+                responsável por criar, modificar e revogar leis. O poder judiciário, por sua vez, é encarregado de interpretar e aplicar as leis, 
+                garantindo a justiça e a conformidade legal. No Brasil, por exemplo, o governo federal está sediado em Brasília e é liderado pelo presidente, 
+                enquanto o Congresso Nacional é composto pelo Senado e pela Câmara dos Deputados. O governo federal desempenha um papel crucial na 
+                formulação de políticas, na defesa dos interesses nacionais e na prestação de serviços públicos essenciais aos cidadãos.</h3>
         </div>
         <div class="patrocinador">
             <img name="" src="/image/Microsoft.jpeg" alt="">
             {{-- <h1 id="imagem">WM</h1> --}}
-            <h3 id="info">Informações</h3>
+            <h3 id="info">A Microsoft é uma das empresas de tecnologia mais proeminentes do mundo, fundada em 1975 por Bill Gates e Paul Allen. 
+                Inicialmente conhecida por seu sistema operacional Windows e pela suíte de produtividade Office, 
+                a empresa expandiu seu alcance para uma ampla gama de produtos e serviços, incluindo hardware, como o console de videogame Xbox, 
+                e serviços em nuvem, como o Microsoft Azure. A Microsoft também é uma líder no desenvolvimento de software empresarial, 
+                com soluções como o Microsoft Dynamics e o Microsoft Teams. Além disso, a empresa está ativamente envolvida em iniciativas de inteligência artificial, 
+                realidade aumentada e computação quântica. Com uma presença global e um histórico de inovação, 
+                a Microsoft continua a desempenhar um papel significativo na forma como as pessoas e as empresas utilizam a tecnologia em suas vidas diárias.</h3>
         </div>
         <div class="patrocinadorDireita">
             <img name="" src="/image/Nike.jpg" alt="">
             {{-- <h1 id="imagem">WM</h1> --}}
-            <h3 id="info">Informações</h3>
+            <h3 id="info">A Nike é uma das maiores empresas de calçados, roupas e acessórios esportivos do mundo. 
+                Fundada em 1964 por Bill Bowerman e Phil Knight, a empresa começou como uma pequena distribuidora de calçados e cresceu para se 
+                tornar um ícone global da indústria esportiva. Reconhecida por seu slogan "Just Do It" e pelo famoso logotipo "Swoosh", 
+                a Nike é conhecida por seus produtos inovadores e de alta qualidade, que são usados por atletas e entusiastas do esporte em todo o mundo. 
+                Além de fabricar uma ampla gama de produtos, a Nike também patrocina atletas de elite e equipes esportivas, 
+                e está envolvida em várias iniciativas de responsabilidade social e ambiental. Com uma presença global e um compromisso com a inovação, 
+                a Nike continua a ser uma das marcas mais reconhecidas e influentes no mundo do esporte e da moda.</h3>
         </div>
         <div class="patrocinador">
             <img name="" src="/image/Porto.jpg" alt="">
             {{-- <h1 id="imagem">WM</h1> --}}
-            <h3 id="info">Informações</h3>
+            <h3 id="info">A Porto Seguro é uma das principais seguradoras do Brasil, fundada em 1945 na cidade de São Paulo. Ao longo dos anos, 
+                a empresa expandiu seu portfólio de produtos para incluir uma ampla gama de seguros, como automóveis, residenciais, 
+                empresariais, de vida e saúde, além de oferecer serviços financeiros, como consórcios e financiamentos. 
+                Reconhecida pela qualidade de seus serviços e pela inovação em soluções de seguro, a Porto Seguro também é conhecida por sua forte 
+                presença no mercado de seguros automotivos, oferecendo benefícios exclusivos e coberturas personalizadas para seus clientes. 
+                Além disso, a empresa investe em tecnologia para melhorar a experiência do cliente, oferecendo aplicativos móveis e canais 
+                digitais para facilitar a contratação e gestão de seguros. Com uma história de sucesso e um compromisso com a excelência, 
+                a Porto Seguro continua a ser uma referência no setor de seguros no Brasil.</h3>
         </div>
         <div class="patrocinadorDireita">
             <img name="" src="/image/SpaceX.jpg" alt="">
             {{-- <h1 id="imagem">WM</h1> --}}
-            <h3 id="info">Informações</h3>
+            <h3 id="info">A SpaceX é uma empresa de tecnologia aeroespacial fundada por Elon Musk em 2002, 
+                com o objetivo de reduzir os custos de lançamento de foguetes e tornar a exploração espacial mais acessível. 
+                Desde então, a SpaceX alcançou uma série de marcos significativos, incluindo o desenvolvimento do foguete Falcon 9 e da cápsula Dragon, 
+                que são usados para transportar carga e tripulação para a Estação Espacial Internacional. 
+                Além disso, a empresa tem trabalhado no desenvolvimento do sistema de lançamento reutilizável, 
+                com o objetivo de tornar os voos espaciais mais econômicos e sustentáveis. A SpaceX também está envolvida em projetos ambiciosos, 
+                como o desenvolvimento do sistema Starship, destinado a transportar humanos para Marte e além, e o projeto Starlink, 
+                que visa fornecer acesso à internet de alta velocidade em todo o mundo através de uma constelação de satélites. 
+                Com sua abordagem inovadora e visão de longo prazo, a SpaceX tem desempenhado um papel crucial na transformação da indústria aeroespacial.</h3>
         </div>
     </div>
  
